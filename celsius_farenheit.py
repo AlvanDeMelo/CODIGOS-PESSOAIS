4ab75fb519004115008aad6abd4ddd2b5283eb35
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-10-05 20:46:40 -0300

    Create celsius_farenheit.py

---
+++ celsius_farenheit.py
@@ -0,0 +1,8 @@
+#Programa do exercício proposto.
+
+celsius = float(input("Entre com a temperatura em Celsius:"))
+
+#fazendo a conversão
+farenheit = (1.8) * celsius + 32
+
+print("A temperatura em Farenheit é: ", farenheit)
