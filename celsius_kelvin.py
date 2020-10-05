9433c46e5be29705fb3eebb36f5fe39ba042819a
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-10-05 20:41:48 -0300

    Create celsius_kelvin.py

---
+++ celsius_kelvin.py
@@ -0,0 +1,8 @@
+#Programa do exercício proposto.
+
+celsius = float(input("Entre com a temperatura em Celsius:"))
+
+#fazendo a conversão
+kelvin = celsius + 273
+
+print("A temperatura em Kelvin é: ", kelvin)
