fd99a5b44f278eb3a2dffa16cb0da1229e7aedb4
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-11-29 14:44:44 -0300

    Create prog_quan_letras.py

---
+++ Pense_em_Python/prog_quan_letras.py
@@ -0,0 +1,17 @@
+#Exibe a palavra
+palavra = input('Insira uma palavra: ')
+print('Sua palavra foi: ', palavra)
+
+#Exibe o total de letras da palavra escolhida
+letras = len(palavra)
+print('O total de letras são:', letras)
+
+#Exibe a primeira letra da palvra
+letras = len(palavra)
+primeira = palavra[:1]
+print('A primeira letra é: ',primeira)
+
+#Exibe a última letra da palavra
+letras = len(palavra)
+ultima = palavra[-1]
+print('A última letra é:',ultima)
