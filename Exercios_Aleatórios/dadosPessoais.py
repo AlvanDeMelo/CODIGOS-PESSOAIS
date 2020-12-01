a93fa1349b539ee303cdd91ef8a0b184ab6efed1
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-11-30 21:46:44 -0300

    Create dadosPessoais.py

---
+++ Exercios_Aleatórios/dadosPessoais.py
@@ -0,0 +1,10 @@
+#Reúne as infromações
+def dados(n,c,i):
+  return(n + c + i)
+
+#Entradas
+n = input("Digite o seu nome: ")
+c = input("Digite o seu CPF: ")
+i = input("Digite a sua idade: ")
+
+print("Nome:",n,". " "CPF:",c,". " "Idade: ",i)
