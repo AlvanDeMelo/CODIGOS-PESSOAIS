f6b7508658c9feec4544e8083533a16a70127b3e
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-11-30 21:55:23 -0300

    Create volume_cilindro.py

---
+++ Exercios_Aleatórios/volume_cilindro.py
@@ -0,0 +1,13 @@
+#Programa do exercício proposto.
+
+#Lê a entrada "Altura"
+h = float(input("Entre com a altura do cilindro:"))
+
+#Lê a entrada "Raio da Base"
+b = float(input("Entre com o raio da base do cilidro:"))
+
+#Executa os calculos
+volume = (3.14) * (b**2) * h
+
+#Exibe o valor
+print("O volume do cilindro é: ", volume)
