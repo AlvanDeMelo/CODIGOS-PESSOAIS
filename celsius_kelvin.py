d9edccd4ff3e86abf788fa56031bfd5e2016e009
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-11-30 21:50:53 -0300

    Delete celsius_kelvin.py

--- celsius_kelvin.py
+++
@@ -1,8 +0,0 @@
-#Programa do exercício proposto.
-
-celsius = float(input("Entre com a temperatura em Celsius:"))
-
-#fazendo a conversão
-kelvin = celsius + 273
-
-print("A temperatura em Kelvin é: ", kelvin)
