83607777cb17d27e30d5056918acf15b370c4b67
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-11-30 21:47:47 -0300

    Delete celsius_farenheit.py

--- celsius_farenheit.py
+++
@@ -1,8 +0,0 @@
-#Programa do exercício proposto.
-
-celsius = float(input("Entre com a temperatura em Celsius:"))
-
-#fazendo a conversão
-farenheit = (1.8) * celsius + 32
-
-print("A temperatura em Farenheit é: ", farenheit)
