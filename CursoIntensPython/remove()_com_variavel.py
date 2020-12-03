29933d4f1284bc6c1d6916f801a25d0af15a7d89
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-12-03 13:18:45 -0300

    Create remove()_com_variavel.py

---
+++ CursoIntensPython/remove()_com_variavel.py
@@ -0,0 +1,16 @@
+#Utilizando o "remove()", com uma variável:
+
+#Lista: 
+nomes = ['maria','manoel','harlan','juliana']
+
+#imprime a lista completa:
+print(nomes)
+
+#Adiciona valor a variável:
+removido = 'manoel'
+
+#Remove o valor adicionado a variável:
+nomes.remove(removido)
+
+#Imprime o valor da lista que foi adicionado a variável:
+print("O nome/item removido foi: ",removido)
