fe20eee2744c84b608d30b3588413dbdafed269c
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-12-02 22:07:42 -0300

    Create alterandoItem.py

---
+++ CursoIntensPython/alterandoItem.py
@@ -0,0 +1,8 @@
+#Para mudar intens de posição/nome numa lista:
+#Lista:
+nomes = ['maria','manoel','harlan','juliana']
+
+#O "[0]" indica a posição na qual o item/nome será substituido:
+nomes[0] = 'alvan'
+
+print(nomes)
