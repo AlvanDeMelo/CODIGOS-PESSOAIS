4820dfd7ef92e7f6530c2a6a4f682c3ed05a585e
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-12-03 12:20:53 -0300

    Create metodo_pop().py

---
+++ CursoIntensPython/metodo_pop().py
@@ -0,0 +1,9 @@
+#O método "pop()" remove o ultimo item/nome de uma lista, mas ainda permite que trabalhe com ele após a remoção.
+
+#Lista: 
+nomes = ['maria','manoel','harlan','juliana']
+
+popped_nomes = nomes.pop()
+
+print(nomes)
+print(popped_nomes)
