e7b133b01d5425454e2a6f687646a18f797bb3ac
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-12-02 22:16:28 -0300

    Create metodoInsert.py

---
+++ CursoIntensPython/metodoInsert.py
@@ -0,0 +1,8 @@
+#O método "insert()" serve para adicionar iten numa posição definida pelo usuário na lista:
+
+#Lista: 
+nomes = ['maria','manoel','harlan','juliana']
+
+nomes.insert(3,'alvan')
+
+print(nomes)
