1d7a8bdd02e8b048928f2da52003c95217bc9246
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-12-02 21:46:41 -0300

    Create saudacao.py

---
+++ CursoIntensPython/saudacao.py
@@ -0,0 +1,10 @@
+#Imprimir nomes/itens de uma lista com a inicial maiúscula e uma saudação/aviso.
+
+#Nomes da lista
+family = ['manoel','maria','soneca','juliana','morpheu','um']
+
+#Saudação
+saudacao = "Você acessou o nome/item: " +family[0].title()+"."
+
+#Imprime
+print(saudacao)
