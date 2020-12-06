2f469bcef2abe4574896c14fd680c18cd0e93de1
AlvanDeMelo <70581483+AlvanDeMelo@users.noreply.github.com>
2020-12-06 13:56:08 -0300

    Create exerc_3.4.py

---
+++ CursoIntensPython/exerc_3.4.py
@@ -0,0 +1,10 @@
+#Exercíco 3.4  - Lista de Convidados.
+
+#Nomes da lista
+convidado = ['manoel','maria','soneca','juliana','morpheu','um']
+
+#Saudação
+saudacao = "Olá " +convidado[2].title()+ "! Você foi convidado para o meu jantar."
+
+#Imprime
+print(saudacao)
