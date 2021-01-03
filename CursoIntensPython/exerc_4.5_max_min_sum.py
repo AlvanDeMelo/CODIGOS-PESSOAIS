cbf97d82b66148245ae38a30c68b3dde0375cf15
Alvan de Melo <70581483+AlvanDeMelo@users.noreply.github.com>
2021-01-03 14:54:20 -0300

    Create exerc_4.5_max_min_sum.py

---
+++ CursoIntensPython/exerc_4.5_max_min_sum.py
@@ -0,0 +1,12 @@
+#Exercício 4.5 somando 1.000.000 e exibindo maximo e minimo
+
+#Lista
+lista = range (1,1000000)
+  
+minimo = min(lista)
+maximo = max(lista)
+soma = sum(lista)
+
+print(minimo)
+print(maximo)
+print(soma)
