0818fe85ddff5b4398174ec6b14939a6d3998feb
Alvan de Melo <70581483+AlvanDeMelo@users.noreply.github.com>
2021-01-03 00:25:10 -0300

    Create exerLaco_1.py

---
+++ CursoIntensPython/exerLaco_1.py
@@ -0,0 +1,9 @@
+#O "Laço for", percorre todos os itens de uma lista.
+
+#Exercício sobre "Laço" com a função "for"
+
+#Lista dos sabores
+pizzas = ['napolitana','baiana','peruana','marguerita']
+for pizza in pizzas:
+  #Exibe os sabores com a primeira letra maíuscula
+  print(pizza.title())
