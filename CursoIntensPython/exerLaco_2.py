b6bf8ab27e8b11bfae8200aa8566337f4a0849b9
Alvan de Melo <70581483+AlvanDeMelo@users.noreply.github.com>
2021-01-03 00:25:59 -0300

    Create exerLaco_2.py

---
+++ CursoIntensPython/exerLaco_2.py
@@ -0,0 +1,7 @@
+#Exercício sobre "Laço" 2
+
+#Lista dos sabores
+pizzas = ['napolitana','baiana','peruana','marguerita']
+for pizza in pizzas:
+  #Exibe os sabores com a primeira letra maíuscula
+  print('Eu gosto do sabor de '+pizza.title()+'.\n')
