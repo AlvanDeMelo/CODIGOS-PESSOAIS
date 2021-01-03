2553e4cd2db273203d2746ae2eceb78a7af28c1d
Alvan de Melo <70581483+AlvanDeMelo@users.noreply.github.com>
2021-01-03 16:07:05 -0300

    Create exerc_4.11_pizzas.py

---
+++ CursoIntensPython/exerc_4.11_pizzas.py
@@ -0,0 +1,16 @@
+#Exercício 4.11 Pizzas minhas e do amigo
+
+#Lista das minhas pizzas
+minhas_pizzas = ['napolitana','baiana','peruana','marguerita']
+#Lista pizzas do amigo
+pizzas_amigo = minhas_pizzas[:]
+
+#Exibe minhas pizzas
+for pizza in minhas_pizzas:
+  #Exibe os sabores com a primeira letra maíuscula
+  print(pizza.title())
+
+#Exibe pizas do amigo
+for pizza in pizzas_amigo:
+  #Exibe os sabores com a primeira letra maíuscula
+  print(pizza.title())
