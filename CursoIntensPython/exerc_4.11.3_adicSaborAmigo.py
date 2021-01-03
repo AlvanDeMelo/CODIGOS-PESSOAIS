fdd53a0d3c3311d6ea8561fb2d8d4c19c8c2bedc
Alvan de Melo <70581483+AlvanDeMelo@users.noreply.github.com>
2021-01-03 16:25:15 -0300

    Create exerc_4.11.3_adicSaborAmigo.py

---
+++ CursoIntensPython/exerc_4.11.3_adicSaborAmigo.py
@@ -0,0 +1,23 @@
+#Exercício 4.11.3 Adicionando sabor a lista do amigo
+
+#Lista das minhas pizzas
+minhas_pizzas = ['napolitana','baiana','peruana','marguerita']
+
+#Adiciona um novo sabor a minha lista
+minhas_pizzas.append('frango com catupiry')
+
+#Lista pizzas do amigo
+pizzas_amigo = minhas_pizzas[:]
+
+#Adiciona um novo sabor a lista do meu amigo
+pizzas_amigo.append('calabresa')
+
+#Exibe minhas pizzas
+for pizza in minhas_pizzas:
+  #Exibe os sabores com a primeira letra maíuscula
+  print(pizza.title())
+
+#Exibe pizas do amigo
+for pizza in pizzas_amigo:
+  #Exibe os sabores com a primeira letra maíuscula
+  print(pizza.title())
