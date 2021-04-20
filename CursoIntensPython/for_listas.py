123faddf778d964100a5ba625aa8abb795f67f52
Alvan de Melo <70581483+AlvanDeMelo@users.noreply.github.com>
2021-04-20 14:22:07 -0300

    Create for_listas.py

---
+++ CursoIntensPython/for_listas.py
@@ -0,0 +1,6 @@
+equested_toppings = ['mushroons', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    print(f"Addinng {requested_topping}.")
+
+print("\nFinished making your pizza!")
