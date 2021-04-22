ef5d070f6238934c50229a94794f91ce5a11229c
Alvan de Melo <70581483+AlvanDeMelo@users.noreply.github.com>
2021-04-22 17:10:20 -0300

    Create modificando_valores_dicionarios.py

---
+++ CursoIntensPython/modificando_valores_dicionarios.py
@@ -0,0 +1,11 @@
+#Modificando o valor:
+
+#Valor definido:
+alien_0 = {'color': 'green'}
+
+print(f"The alien is {alien_0['color']}.")
+
+#Muda o valor:
+alien_0['color'] = 'yellow'
+
+print(f"The alien is now {alien_0['color']}.")
