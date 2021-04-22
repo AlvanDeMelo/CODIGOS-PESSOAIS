7660f48be1046f8c8929c4b8383bfe55be3f3896
Alvan de Melo <70581483+AlvanDeMelo@users.noreply.github.com>
2021-04-22 15:42:22 -0300

    Create 5.11_numeros_ordinais.py

---
+++ CursoIntensPython/5.11_numeros_ordinais.py
@@ -0,0 +1,13 @@
+#Números ordinais em inglês
+
+#Lista de números:
+numeros = ['1', '2','3', '4', '5', '6', '7', '8', '10']
+
+for numero in numeros:
+    if numero == '1':
+        print('1st')
+    elif numero == '2':
+        print("2nd")
+    elif numero == '3':
+        print('3rd')
+    else: print(f'{numero}th')
